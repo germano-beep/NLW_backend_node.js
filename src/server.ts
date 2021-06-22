039dc5ffab9e752cca4714bb441c0cda4fb8f9c3
arthur <garthur83@gmail.com>
2021-06-22 10:08:07 -0300

    primeiro commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import "reflect-metadata";
+import express, { request, response } from "express";
+import { router } from "./routes";
+
+import "./database";
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(3000, () => console.log("Server is running"));
